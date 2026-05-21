3cc1b5306c7d81014298cfb90f06f0cf518ed9cc
1056682585 <1056682585@qq.com>
2026-05-21 16:42:53 +0800

    本地提交

---
+++ src/test/java/com/example/allsystem/AllSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.allsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
